b6813f0f844b5dea7d0b5c7f591a0b6357328ce5
Jorad <joraddevries@gmail.com>
2026-01-08 12:04:46 +0100

    Routine commit

--- R/run_app.R
+++ R/run_app.R
@@ -2,7 +2,7 @@
 #' @export
 
 run_sgvm <- function() {
-  app_dir <- system.file("shiny", package = "sgvm")
+  app_dir <- system.file("shiny", package = "SGVM")
   if (app_dir == "") stop("Could not find app directory. Try reinstalling the package.")
   shiny::runApp(app_dir, launch.browser = TRUE)
 }
